20598747e61a8e87c16a81d9f95ceb5f31ce4c3e
Erik Brandsma <erik.brandsma@outlook.com>
2016-04-29 14:37:46 +0200

    0.3

--- stringreturner-lib/src/main/java/mma/afdelingapps/com/stringreturner/ReturnerClass.java
+++ stringreturner-lib/src/main/java/mma/afdelingapps/com/stringreturner/ReturnerClass.java
@@ -1,7 +1,12 @@
 package mma.afdelingapps.com.stringreturner;
 
+import mma.afdelingapps.com.stringextender.ExtenderClass;
+
 /**
  * Created by Erik on 29-Apr-16.
  */
 public class ReturnerClass {
+    public static String returnSomething(){
+        return "HOI + " + ExtenderClass.extend();
+    }
 }
